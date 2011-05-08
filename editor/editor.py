0513882349a5e4bbd349285cc473a1ab5ef05752
ryan <ryan@moreharts.com>
2011-05-08 22:58:33 +0000

    Switched to GTK and GTKSourceView

--- editor/editor.py
+++ editor/editor.py
@@ -1,46 +1,64 @@
 #!/usr/bin/python -3
 import sys
-from PyQt4 import QtGui
+import gtk
 from gui import EditorWindow
 
-class SparseEdit(QtGui.QApplication):
+class SparseEdit:
 	def __init__(self, argv):
-		super(SparseEdit, self).__init__(argv)
-		
-		self.__windows = []
+		self.__editors = []
 		self.__project = None
 		
 		# Are there any files given on the command line?
-		# TODO
-		self.new_file()
 		
-	def open_file(self, filename):
+	
+	def start(self):
+		"""Runs GTK's main loop, starting the event sytem"""
+		gtk.main()	
+	
+	##############################
+	# File management functions
+	def openFile(self, filename):
 		"""Opens the given file"""
-		new_win = EditorWindow(self, filename)
-		self.__windows.append(new_win)
-		new_win.show()
+		newWin = EditorWindow(self, filename)
+		self.__editors.append(newWin)
+		newWin.show()
 	
-	def new_file(self):
+	def newFile(self):
 		"""Creates a new editor window that is not pointed to anything"""
-		new_win = EditorWindow(self)
-		self.__windows.append(new_win)
-		new_win.show()
+		newWin = EditorWindow(self)
+		self.__editors.append(newWin)
+		newWin.show()
 	
-	def is_project_open(self):
+	###############################
+	# Window management functions
+	def stopManagingEditor(self, editor):
+		"""
+		Removes the given editor from the list of editor windows we have
+		open. If this was the last window we had, stop execution
+		"""
+		self.__editors.remove(editor)
+		
+		# Should we finish and close completely?
+		if self.__editors == []:
+			gtk.main_quit()
+	
+	def isProjectOpen(self):
 		"""Returns True if a project is currently open"""
 		if self.__project is None:
 			return False
 		else:
 			return True
 	
-	def project_name(self):
+	def projectName(self):
 		"""
 		Returns the name of the current project or None if there is no
 		project.
 		"""
 		# TODO
 		return "Blah"
-	
+
+#######################
+# Main
 def main(argv = None):
 	"""Primary startup point for the editor"""
 	# We have the default as None rather than sys.argv
@@ -50,8 +68,8 @@ def main(argv = None):
         
     # Run application
 	app = SparseEdit(sys.argv)
-	return app.exec_()
+	app.start()
 	
 if __name__ == '__main__':
-	sys.exit(main(sys.argv))
+	main(sys.argv)
 
--- editor/gui.py
+++ editor/gui.py
@@ -2,48 +2,126 @@
 Includes majority of GUI code for SparseEdit
 """
 
-from PyQt4 import QtGui
-from PyQt4.Qsci import QsciScintilla
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gtksourceview2 as gtksourceview
 
-class EditorWindow(QtGui.QWidget):
+class EditorWindow(gtk.Window):
 	"""
 	Editor window that handles majority of interaction with user. Currently
 	only handles a single file at a time. Multiple windows will be used for
 	editing	multiple files
 	"""
 	
-	def __init__(self, editor_instance, filename = None):
+	def __init__(self, editor, filename = None):
 		"""
 		Creates new editor window, but does not automatically show itself.
 		"""
-		super(EditorWindow, self).__init__(None)
+		gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
 		
 		# Save off reference to the running instance we're a part of
-		self.__editor = editor_instance
-		self.__file_name = filename
+		self.__editor = editor
+		self.__filename = None
+		self.__isChanged = False
 		
 		# Basic window stuff
+		self.set_default_size(500, 500)
+		self.__mainBox = gtk.VBox(False, 0)
+		self.add(self.__mainBox)
+		self.__mainBox.show()
 		self.__update_title()
-			
-		# Create editor area
 		
+		# Main events
+		self.connect("destroy", self.destroy)
 		
+		# Create editor area and attach the main handler we'll be relying on
+		self.__text = gtksourceview.View()
+		self.__mainBox.pack_start(self.__text, True, True, 0)
+		self.__text.show()
+		
+		self.__text.connect('key_press_event', self.keyPressed)
+		
+		# Create search area (do not show, only activates on shortcut key)
+		self.__searchAreaTable = gtk.Table(2, 2, False)
+		self.__mainBox.pack_start(self.__searchAreaTable, False, False, )
+		
+		# Search area
+		searchLabel = gtk.Label("Find:")
+		searchLabel.set_justify(gtk.JUSTIFY_RIGHT)
+		self.__searchAreaTable.attach(searchLabel, 0, 1, 0, 1, gtk.FILL)
+		searchLabel.show()
+		
+		self.__searchEntry = gtk.Entry()
+		self.__searchAreaTable.attach(self.__searchEntry, 1, 2, 0, 1, gtk.EXPAND | gtk.FILL)
+		self.__searchEntry.show()
+		
+		# Replace area
+		replaceLabel = gtk.Label("Replace:")
+		replaceLabel.set_justify(gtk.JUSTIFY_RIGHT)
+		self.__searchAreaTable.attach(replaceLabel, 0, 1, 1, 2, 0, gtk.FILL)
+		replaceLabel.show()
+		
+		self.__replaceEntry = gtk.Entry()
+		self.__searchAreaTable.attach(self.__replaceEntry, 1, 2, 1, 2, gtk.EXPAND | gtk.FILL)
+		self.__replaceEntry.show()
+		
+		# Do we have a file to open?
+		if self.__filename is not None:
+			self.openFile(filename)
+		
+	def destroy(self, widget, data = None):
+		"""
+		Kills the window and lets the editor manager know we're dead
+		"""
+		self.__editor.stopManagingEditor(self)
+	
+	def keyPressed(self, widget, data = None):
+		"""
+		Watches for all the cool
+		"""
+		print(widget)
+		pass
+	
+	def openFile(filename):
+		"""
+		Opens the given file. Prompts the user if the current file is unsaved
+		"""
+		if self.__isChanged:
+			pass
+		
+		print("Opening " + filename)
+		
+		# Read in new file
+		self.__filename = filename
+		f = open(self.__filename, 'r')
+		buf = self.__text.get_buffer()
+		buf.set_text(f.read())
+		f.close()
+		
+		self.__isChanged = False
+		
+		# Ensure our title is correct
+		self.__update_title()
+	
 	def __update_title(self):
 		"""
 		Ensures that the window title is correct based on the current project
 		and open file name
 		"""
-		if self.__editor.is_project_open():
-			title = self.__editor.project_name() + ' - '
+		if self.__isChanged:
+			title = '* '
 		else:
 			title = ''
+		
+		if self.__editor.isProjectOpen():
+			title += self.__editor.projectName() + ' - '
 	
-		if self.__file_name is None:
+		if self.__filename is None:
 			title += "New File"
 		else:
 			title += self.__file_name
 			
 		# Actually set the title
-		self.setWindowTitle(title)
-			
+		self.set_title(title)
 			
