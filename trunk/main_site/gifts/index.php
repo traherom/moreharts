3ec7103caf814015b8b49babd6a252e9c4a836ce
ryan@moreharts.com <ryan@moreharts.com@fa5ea75b-a228-d56f-84e4-a555cbe47481>
2011-05-10 15:15:28 +0000

    Many of the pages for the gift exchange site are in place

--- trunk/main_site/gifts/index.php
+++ trunk/main_site/gifts/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1"> 
+	
 	<title>Gift Exachanger</title>
 	
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
@@ -10,6 +13,35 @@
 	<script src="main.js"></script>
 </head>
 <body>
-
+	<div data-role="page" id="home">
+		<div  data-role="header">Gift Exchange</div> 
+		<div  data-role="content">...</div> 
+		<div  data-role="footer">...</div> 
+	</div>
+	<div data-role="page" id="login">
+		<div  data-role="header">Login</div> 
+		<div  data-role="content">...</div> 
+		<div  data-role="footer">...</div> 
+	</div>
+	<div data-role="page" id="create-group">
+		<div  data-role="header">...</div> 
+		<div  data-role="content">...</div> 
+		<div  data-role="footer">...</div> 
+	</div>
+	<div data-role="page" id="view-group">
+		<div  data-role="header">...</div> 
+		<div  data-role="content">...</div> 
+		<div  data-role="footer">...</div> 
+	</div>
+	<div data-role="page" id="create-member">
+		<div  data-role="header">...</div> 
+		<div  data-role="content">...</div> 
+		<div  data-role="footer">...</div> 
+	</div>
+	<div data-role="page" id="view-member">
+		<div  data-role="header">...</div> 
+		<div  data-role="content">...</div> 
+		<div  data-role="footer">...</div> 
+	</div>
 </body>
 </html>
