ab854ed7b883b042b5cbc6fc8c6bd1d33932e214
ryan <ryan@moreharts.com>
2011-05-10 14:09:03 +0000

    Began adding gift site

---
+++ main_site/gifts/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gift Exachanger</title>
+	
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+	
+	<script src="main.js"></script>
+</head>
+<body>
+
+</body>
+</html>
