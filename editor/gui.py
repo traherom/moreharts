59c80a4ffcaaafa2cda8c8d4e82f58f27dc27de7
ryan <ryan@moreharts.com>
2011-05-11 00:00:41 +0000

    Find and replace both work

--- editor/gui.py
+++ editor/gui.py
@@ -2,6 +2,7 @@ import pygtk
 pygtk.require('2.0')
 from gi.repository import Gtk, GtkSource, Gdk
 import mimetypes
+import re
 
 # There are big dereferencing problems with the language manager,
 # it helps to just have the one instance out here
@@ -64,6 +65,7 @@ class EditorWindow(Gtk.Window):
 		searchLabel.show()
 		
 		self.__searchEntry = Gtk.Entry()
+		self.__searchEntry.connect('key-press-event', self.search_key_pressed)
 		self.__searchAreaTable.attach(self.__searchEntry, 1, 2, 0, 1,
 			Gtk.AttachOptions.EXPAND | Gtk.AttachOptions.FILL, Gtk.AttachOptions.FILL, 0, 0)
 		self.__searchEntry.show()
@@ -76,9 +78,16 @@ class EditorWindow(Gtk.Window):
 		self.__replaceLabel.show()
 		
 		self.__replaceEntry = Gtk.Entry()
+		self.__replaceEntry.connect('key-press-event', self.replace_key_pressed)
 		self.__searchAreaTable.attach(self.__replaceEntry, 1, 2, 1, 2,
 			Gtk.AttachOptions.EXPAND | Gtk.AttachOptions.FILL, Gtk.AttachOptions.FILL, 0, 0)
 		self.__replaceEntry.show()
+		
+		# Status bar
+		self.__status = Gtk.Statusbar()
+		self.__mainBox.pack_start(self.__status, False, False, 0)
+		self.__status.show()
+		self.set_status('Welcome to SparseEdit')
 		
 		# Basically ready to go, start accepting keys
 		self.connect('key_press_event', self.key_pressed)
@@ -89,7 +98,10 @@ class EditorWindow(Gtk.Window):
 			
 		# Ensure the title is correct
 		self.__update_title()
-		
+	
+	#######################
+	# Events
+	
 	def destroy(self, widget, data = None):
 		"""
 		Kills the window and lets the editor manager know we're dead
@@ -130,6 +142,9 @@ class EditorWindow(Gtk.Window):
 			# Esc, close anything that's open other than the main editing window
 			self.hide_search()
 	
+	#############################
+	# File utilities
+	
 	def open_file(self, filename):
 		"""
 		Opens the given file. Prompts the user if the current file is unsaved
@@ -149,8 +164,10 @@ class EditorWindow(Gtk.Window):
 		self.__buffer.end_not_undoable_action()
 		self.__buffer.set_modified(False)
 		
+		# Move cursor to start of file
+		self.__buffer.place_cursor(self.__buffer.get_start_iter())
+		
 		# Highlighting mode based on file opened
-		#print('Mode:', self.detect_language())
 		self.set_language(self.detect_language())
 		
 		# Ensure our title is correct
@@ -184,6 +201,9 @@ class EditorWindow(Gtk.Window):
 		"""Redoes a single step"""
 		self.__buffer.redo()
 	
+	###########################
+	# Find and replace
+	
 	def open_search(self):
 		"""
 		Opens the search box, hiding the replacement components
@@ -209,13 +229,111 @@ class EditorWindow(Gtk.Window):
 		"""
 		Open the search and replace box
 		"""
-		# Hide the replace stuff
+		# Show the replace stuff
 		self.__replaceLabel.show()
 		self.__replaceEntry.show()
+		self.__replaceEntry.grab_focus()
 		
-		# Show the search area and focus the keyboard
+		# Show the search area
 		self.__searchAreaTable.show()
-		self.__searchEntry.grab_focus()
+	
+	def search_key_pressed(self, widget, event):
+		"""
+		Search when the user hits enter
+		"""
+		data = event.key
+		if data.keyval == Gdk.KEY_Return:
+			self.do_search(self.__searchEntry.get_text())
+			
+	def replace_key_pressed(self, widget, event):
+		"""
+		Search and replace when the user hits enter
+		"""
+		data = event.key
+		if data.keyval == Gdk.KEY_Return:
+			self.do_replace(self.__searchEntry.get_text(), self.__replaceEntry.get_text())
+			
+	def do_search(self, find_regex = None, loop_end = None):
+		"""
+		Find next instance of given regex in source buffer, starting from 
+		where the cursor is currently located. If no regex is given, 
+		the current match object is used (assuming there is one)
+		"""
+		if find_regex is not None:
+			try:
+				self.__currRegex = re.compile(find_regex)
+			except re.error as e:
+				self.set_status('Search error: ' + str(e))
+				return None
+		
+		# Start next match from the current cursor location
+		# If there is a selection (possibly from our previous a search),
+		# start one beyond the first part of it
+		start = self.__buffer.get_iter_at_mark(self.__buffer.get_insert()).get_offset()
+		selPoint = self.__buffer.get_iter_at_mark(self.__buffer.get_selection_bound()).get_offset()
+		if start < selPoint:
+			start += 1
+		elif selPoint < start:
+			start = selPoint + 1
+		
+		while True:
+			match = self.__currRegex.search(self.__buffer.get_text(
+					self.__buffer.get_start_iter(), self.__buffer.get_end_iter(), True),
+					start)
+		
+			if match is not None:
+				# Select from the start to the end of match, if we found one
+				startIter = self.__buffer.get_iter_at_offset(match.start())
+				self.__buffer.select_range(
+					startIter,
+					self.__buffer.get_iter_at_offset(match.end()))
+					
+				self.set_status('Match found on line ' + str(startIter.get_line()))
+				return match.start()
+				
+			elif start > 0:
+				# No match found, start search from beginning
+				self.set_status('No match found, looping from beginning')
+				start = 0
+			
+			else:
+				# No match found, even from beginning
+				self.set_status('No match found')
+				return None
+	
+	def do_replace(self, find_regex = None, replace_regex = None, loop_end = None):
+		"""
+		Find next instance of given regex in source buffer and replaces it with
+		the replacement regex, starting from where the cursor is currently
+		located. If no regex is given, the current match object is used
+		(assuming there is one)
+		"""
+		startIndex = self.do_search(find_regex)
+		if startIndex is not None:
+			# Found a match, replace selection with replacement text
+			endIndex = self.__buffer.get_iter_at_mark(self.__buffer.get_selection_bound()).get_offset()
+			
+			# This would be ideal, but it seems buggy:
+			# self.__buffer.delete_selection(False, True)
+			# self.__buffer.insert_at_cursor('test')
+			
+			text = self.__buffer.get_text(self.__buffer.get_start_iter(),
+				self.__buffer.get_end_iter(), True)
+			newText = text[:startIndex] + replace_regex + text[endIndex:]
+			
+			# Send back to buffer and select insertion
+			self.__buffer.set_text(newText, len(newText))
+			self.__buffer.select_range(
+				self.__buffer.get_iter_at_offset(startIndex),
+				self.__buffer.get_iter_at_offset(startIndex + len(replace_regex)))
+			return True
+			
+		else:
+			self.set_status('No matches to replace')
+			return False
+	
+	###########################
+	# Highlighting
 	
 	def set_language(self, lang):
 		"""
@@ -226,7 +344,6 @@ class EditorWindow(Gtk.Window):
 		self.__buffer.set_highlight_syntax(False)
 		
 		if lang is not None:
-			print("Language: ", lang.get_style_ids(), lang.get_hidden(), lang.get_name())
 			self.__buffer.set_language(lang)
 			self.__buffer.set_highlight_syntax(True)
 		
@@ -241,6 +358,9 @@ class EditorWindow(Gtk.Window):
 			mime = mimetypes.guess_type(self.__filename)[0]
 			lang = lang_manager.guess_language(self.__filename, mime)
 			return lang
+	
+	####################
+	# UI updates
 	
 	def __update_title(self):
 		"""
@@ -265,3 +385,10 @@ class EditorWindow(Gtk.Window):
 		# Actually set the title
 		self.set_title(title)
 
+	def set_status(self, msg):
+		"""
+		Sets the status to the given message
+		"""
+		cid = self.__status.get_context_id('main')
+		self.__status.push(cid, msg)
+		
