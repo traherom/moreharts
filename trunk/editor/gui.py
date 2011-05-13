964bd918a9f01574e963e0cfefa47805c332d02c
ryan@moreharts.com <ryan@moreharts.com@fa5ea75b-a228-d56f-84e4-a555cbe47481>
2011-05-13 01:38:54 +0000

    - Added ctrl-tabbing between windows - Added menubar hiding and showing on alt/esc - Made find default to current selection - Add undo/redo to status bar - Made line/column number display in the status bar - Added fullscreen support via alt-enter

--- trunk/editor/gui.py
+++ trunk/editor/gui.py
@@ -24,25 +24,117 @@ class EditorWindow(Gtk.Window):
 		# Save off reference to the running instance we're a part of
 		self.__editor = editor
 		self.__filename = filename
+		self.__isFullscreen = False
 		
 		# Basic window stuff
 		self.set_default_size(500, 500)
-		self.__mainBox = Gtk.VBox(homogeneous = False, spacing = 0)
+		self.connect('window-state-event', self.__window_state_change)
+		self.__mainBox = Gtk.VBox(homogeneous = False, spacing = 3)
 		self.add(self.__mainBox)
 		self.__mainBox.show()
 		
 		# Main events
-		self.connect("destroy", self.destroy)
+		self.connect("destroy", self.close)
+		
+		# Menubar (hidden by default)
+		self.__menubar = Gtk.MenuBar()
+		self.__mainBox.pack_start(self.__menubar, False, False, 0)
+		
+		# File menu
+		fileMenuTop = Gtk.MenuItem(label = 'File')
+		self.__menubar.append(fileMenuTop)
+		fileMenuTop.show()
+		
+		fileMenu = Gtk.Menu()
+		fileMenuTop.set_submenu(fileMenu)
+		item = Gtk.MenuItem(label = 'New')
+		fileMenu.append(item)
+		item.show()
+		item.connect('activate', self.save_file)
+		
+		item = Gtk.MenuItem(label = 'Open')
+		fileMenu.append(item)
+		item.show()
+		item.connect('activate', self.save_file)
+		
+		item = Gtk.SeparatorMenuItem()
+		fileMenu.append(item)
+		item.show()
+		
+		item = Gtk.MenuItem(label = 'Save')
+		fileMenu.append(item)
+		item.show()
+		item.connect('activate', self.save_file)
+		
+		item = Gtk.MenuItem(label = 'Save As...')
+		fileMenu.append(item)
+		item.show()
+		item.connect('activate', self.save_file_as)
+		
+		item = Gtk.SeparatorMenuItem()
+		fileMenu.append(item)
+		item.show()
+		
+		item = Gtk.MenuItem(label = 'Close')
+		fileMenu.append(item)
+		item.show()
+		item.connect('activate', self.close)
+		
+		# Edit menu
+		editMenuTop = Gtk.MenuItem(label = 'Edit')
+		self.__menubar.append(editMenuTop)
+		editMenuTop.show()
+		
+		editMenu = Gtk.Menu()
+		editMenuTop.set_submenu(editMenu)
+		item = Gtk.MenuItem(label = 'Undo')
+		editMenu.append(item)
+		item.show()
+		item.connect('activate', self.undo)
+		
+		item = Gtk.MenuItem(label = 'Redo')
+		editMenu.append(item)
+		item.show()
+		item.connect('activate', self.redo)
+		
+		item = Gtk.SeparatorMenuItem()
+		editMenu.append(item)
+		item.show()
+		
+		item = Gtk.MenuItem(label = 'Cut')
+		editMenu.append(item)
+		item.show()
+		item.connect('activate', self.cut)
+		
+		item = Gtk.MenuItem(label = 'Copy')
+		editMenu.append(item)
+		item.show()
+		item.connect('activate', self.copy)
+		
+		item = Gtk.MenuItem(label = 'Paste')
+		editMenu.append(item)
+		item.show()
+		item.connect('activate', self.paste)
+		
+		# Project
+		projectMenuTop = Gtk.MenuItem(label = 'Project')
+		self.__menubar.append(projectMenuTop)
+		projectMenuTop.show()
 		
 		# Create editor area and attach the main handler we'll be relying on
 		self.__text = GtkSource.SourceView(buffer = GtkSource.SourceBuffer())
 		self.__text.set_show_line_numbers(True)
 		self.__text.set_auto_indent(True)
 		self.__text.set_smart_home_end(GtkSource.SourceSmartHomeEndType.ALWAYS)
+		
 		self.__buffer = self.__text.get_buffer()
 		self.__buffer.set_highlight_matching_brackets(True)
 		self.__buffer.set_max_undo_levels(200)
 		self.__buffer.connect('modified-changed', self.__buffer_changed)
+		self.__buffer.connect('mark-set', self.__cursor_moved)
+		
+		#self.__clipboard = Gtk.Clipboard.get(Gdk.Atom.intern('PRIMARY'))
+		#print(self.__clipboard)
 		
 		scroll = Gtk.ScrolledWindow()
 		scroll.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
@@ -52,7 +144,6 @@ class EditorWindow(Gtk.Window):
 		scroll.show()
 		
 		# Create search area (do not show, only activates on shortcut key)
-		#self.__searchAreaTable = Gtk.Table(rows = 2, columns = 2, homogeneous = False)
 		self.__searchAreaTable = Gtk.Table()
 		self.__searchAreaTable.resize(2, 2)
 		self.__mainBox.pack_start(self.__searchAreaTable, False, False, 0)
@@ -65,7 +156,7 @@ class EditorWindow(Gtk.Window):
 		searchLabel.show()
 		
 		self.__searchEntry = Gtk.Entry()
-		self.__searchEntry.connect('key-press-event', self.search_key_pressed)
+		self.__searchEntry.connect('activate', self.search_key_pressed)
 		self.__searchAreaTable.attach(self.__searchEntry, 1, 2, 0, 1,
 			Gtk.AttachOptions.EXPAND | Gtk.AttachOptions.FILL, Gtk.AttachOptions.FILL, 0, 0)
 		self.__searchEntry.show()
@@ -78,7 +169,7 @@ class EditorWindow(Gtk.Window):
 		self.__replaceLabel.show()
 		
 		self.__replaceEntry = Gtk.Entry()
-		self.__replaceEntry.connect('key-press-event', self.replace_key_pressed)
+		self.__replaceEntry.connect('activate', self.replace_key_pressed)
 		self.__searchAreaTable.attach(self.__replaceEntry, 1, 2, 1, 2,
 			Gtk.AttachOptions.EXPAND | Gtk.AttachOptions.FILL, Gtk.AttachOptions.FILL, 0, 0)
 		self.__replaceEntry.show()
@@ -87,6 +178,11 @@ class EditorWindow(Gtk.Window):
 		self.__status = Gtk.Statusbar()
 		self.__mainBox.pack_start(self.__status, False, False, 0)
 		self.__status.show()
+		
+		self.__locationLbl = Gtk.Label()
+		self.__status.pack_start(self.__locationLbl, False, False, 0)
+		self.__locationLbl.show()
+		
 		self.set_status('Welcome to SparseEdit')
 		
 		# Basically ready to go, start accepting keys
@@ -100,17 +196,54 @@ class EditorWindow(Gtk.Window):
 		self.__update_title()
 	
 	#######################
-	# Events
+	# Main events
 	
-	def destroy(self, widget, data = None):
+	def close(self, widget = None, data = None):
 		"""
 		Kills the window and lets the editor manager know we're dead
 		"""
 		self.__editor.stop_managing_editor(self)
 	
+	def __window_state_change(self, widget, event):
+		"""
+		Keeps track of whether the window is full screen or not
+		"""
+		if event.window_state.new_window_state & Gdk.WindowState.FULLSCREEN != 0:
+			self.__isFullscreen = True
+		else:
+			self.__isFullscreen = False
+	
 	def __buffer_changed(self, widget):
-		""" Catches when the buffer is changed """
+		"""
+		Catches when the buffer is changed
+		"""
 		self.__update_title()
+	
+	def __cursor_moved(self, widget, newIter, newMark):
+		"""
+		Ensures that the current line/column display is correct
+		"""
+		cursorIter = self.__buffer.get_iter_at_mark(self.__buffer.get_insert())
+		self.__locationLbl.set_text('Ln ' + str(cursorIter.get_line() + 1)
+			+ ', Col ' + str(cursorIter.get_line_offset() + 1))
+	
+	def cut(self, ignored = None):
+		"""
+		Cuts the current selection to the clipboard
+		"""
+		self.__buffer.cut_clipboard(self.__clipboard)
+		
+	def copy(self, ignored = None):
+		"""
+		Copies the current selection to the clipboard
+		"""
+		self.__buffer.cut_clipboard(self.__clipboard)
+		
+	def paste(self, ignored = None):
+		"""
+		Pastes the clipboard to the cursor
+		"""
+		self.__buffer.cut_clipboard(self.__clipboard, None, True)
 	
 	def key_pressed(self, widget, event):
 		"""
@@ -131,16 +264,36 @@ class EditorWindow(Gtk.Window):
 				self.open_replace()
 			elif data.keyval == Gdk.KEY_f:
 				self.open_search()
+			elif data.keyval == Gdk.KEY_w:
+				self.close()
+			elif data.keyval == Gdk.KEY_n:
+				self.__editor.new_file()
+			elif data.keyval == Gdk.KEY_g:
+				self.__searchEntry.activate()
+			elif data.keyval == Gdk.KEY_Return:
+				if self.__isFullscreen:
+					self.unfullscreen()
+				else:
+					self.fullscreen()
+			elif data.keyval == Gdk.KEY_Tab:
+				self.__editor.switch_to_next_editor(self)
 				
-		elif data.state & Gdk.ModifierType.SHIFT_MASK != 0:
-			# Shift+...
-			print("Shift+" + str(data.keyval))
 		elif data.state & Gdk.ModifierType.MOD1_MASK != 0:
 			# Alt+...
 			print("Alt+" + str(data.keyval))
+		
+		elif data.keyval == Gdk.KEY_Alt_L or data.keyval == Gdk.KEY_Alt_R:
+			# Alt by itself
+			self.show_menubar()
+		
 		elif data.keyval == Gdk.KEY_Escape:
 			# Esc, close anything that's open other than the main editing window
 			self.hide_search()
+			self.hide_menubar()
+			
+		elif data.keyval == Gdk.KEY_F3:
+			# Re-search, just as if the user hit 'enter' on the search box
+			self.__searchEntry.activate()
 	
 	#############################
 	# File utilities
@@ -150,7 +303,8 @@ class EditorWindow(Gtk.Window):
 		Opens the given file. Prompts the user if the current file is unsaved
 		"""
 		if self.__buffer.get_modified():
-			pass
+			self.set_status('Save current file first')
+			return
 		
 		# Read in new file
 		self.__filename = filename
@@ -173,33 +327,49 @@ class EditorWindow(Gtk.Window):
 		# Ensure our title is correct
 		self.__update_title()
 	
-	def save_file(self):
+	def save_file_as(self, ignored = None):
+		"""
+		Prompts the user for where to save the current file 
+		"""
+		chooser = Gtk.FileChooserDialog("Save As...", self,
+			Gtk.FileChooserAction.SAVE,
+			('Save', 0, 'Cancel', 1))
+		chooser.set_do_overwrite_confirmation(True)
+		#chooser.connect('', )
+		chooser.show()
+		
+	def save_file(self, ignored = None):
 		"""
 		Saves the current file to disk. If there is no filename currently set,
 		prompts the user for one
 		"""
 		if self.__filename is None:
-			chooser = Gtk.FileChooserDialog("Save As...", self,
-				Gtk.FileChooserAction.SAVE,
-				('Save', 0, 'Cancel', 1))
-			chooser.set_do_overwrite_confirmation(True)
-			#chooser.connect('', )
-			chooser.show()
+			self.save_file_as()
 			return
 		
 		f = open(self.__filename, 'w')
 		f.write(self.__buffer.get_text(self.__buffer.get_start_iter(), self.__buffer.get_end_iter(), True))
 		f.close()
 		
+		self.set_status('Saved to ' + self.__filename)
+			
 		self.__buffer.set_modified(False)
 	
 	def undo(self):
 		"""Undoes a single step"""
-		self.__buffer.undo()
+		if self.__buffer.can_undo():
+			self.__buffer.undo()
+			self.set_status('Undo')
+		else:
+			self.set_status('Nothing to undo')
 		
 	def redo(self):
 		"""Redoes a single step"""
-		self.__buffer.redo()
+		if self.__buffer.can_redo():
+			self.__buffer.redo()
+			self.set_status('Redo')
+		else:
+			self.set_status('Nothing to redo')
 	
 	###########################
 	# Find and replace
@@ -208,6 +378,26 @@ class EditorWindow(Gtk.Window):
 		"""
 		Opens the search box, hiding the replacement components
 		"""
+		# Is there a current selection? If so, use that as the literal search term
+		if self.__buffer.get_has_selection():
+			ignored, startIter, endIter = self.__buffer.get_selection_bounds()
+			searchText = self.__buffer.get_text(startIter, endIter, True)
+			
+			# Do some escaping to make it be interpreted literally, not as a regex
+			searchText = searchText.replace('\\', '\\\\')
+			searchText = searchText.replace('.', '\\.')
+			searchText = searchText.replace('*', '\\*')
+			searchText = searchText.replace('?', '\\?')
+			searchText = searchText.replace('+', '\\+')
+			searchText = searchText.replace('(', '\\(')
+			searchText = searchText.replace(')', '\\)')
+			searchText = searchText.replace('[', '\\[')
+			searchText = searchText.replace('^', '\\^')
+			searchText = searchText.replace('|', '\\|')
+			
+			# And use it
+			self.__searchEntry.set_text(searchText)
+		
 		# Hide the replace stuff
 		self.__replaceLabel.hide()
 		self.__replaceEntry.hide()
@@ -229,29 +419,24 @@ class EditorWindow(Gtk.Window):
 		"""
 		Open the search and replace box
 		"""
+		self.open_search()
+		
 		# Show the replace stuff
 		self.__replaceLabel.show()
 		self.__replaceEntry.show()
 		self.__replaceEntry.grab_focus()
-		
-		# Show the search area
-		self.__searchAreaTable.show()
 	
-	def search_key_pressed(self, widget, event):
+	def search_key_pressed(self, widget):
 		"""
 		Search when the user hits enter
 		"""
-		data = event.key
-		if data.keyval == Gdk.KEY_Return:
-			self.do_search(self.__searchEntry.get_text())
+		self.do_search(self.__searchEntry.get_text())
 			
-	def replace_key_pressed(self, widget, event):
+	def replace_key_pressed(self, widget):
 		"""
 		Search and replace when the user hits enter
 		"""
-		data = event.key
-		if data.keyval == Gdk.KEY_Return:
-			self.do_replace(self.__searchEntry.get_text(), self.__replaceEntry.get_text())
+		self.do_replace(self.__searchEntry.get_text(), self.__replaceEntry.get_text())
 			
 	def do_search(self, find_regex = None, loop_end = None):
 		"""
@@ -358,6 +543,21 @@ class EditorWindow(Gtk.Window):
 			mime = mimetypes.guess_type(self.__filename)[0]
 			lang = lang_manager.guess_language(self.__filename, mime)
 			return lang
+	
+	####################
+	# Menu
+	
+	def show_menubar(self):
+		"""
+		Displays the menu bar to the user
+		"""
+		self.__menubar.show()
+		
+	def hide_menubar(self):
+		"""
+		Hides the menubar from the user
+		"""
+		self.__menubar.hide()
 	
 	####################
 	# UI updates
