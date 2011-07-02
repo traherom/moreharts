7f63cb076b17fd32201cf8f52e37580ec9295227
ryan <ryan@moreharts.com>
2011-07-02 02:21:03 +0000

    Base project for msfdroid

---
+++ msfdroid/src/com/moreharts/android/msfdroid/MainListing.java
@@ -0,0 +1,15 @@
+package com.moreharts.android.msfdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainListing extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
