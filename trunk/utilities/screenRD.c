5a166130f4210c5c5c3d61a220bef9533dcd3b27
ryan@moreharts.com <ryan@moreharts.com@fa5ea75b-a228-d56f-84e4-a555cbe47481>
2011-11-01 12:23:45 +0000

    Fixed up screenRD to work with sftp

--- trunk/utilities/screenRD.c
+++ trunk/utilities/screenRD.c
@@ -1,9 +1,28 @@
+/****************
+Simple utility to allow you to use
+screen as your shell without screwing 
+up sftp (basically screen does not handle
+-c very well).
+****************/
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
 	char *screen = "/usr/bin/screen";
-	execl(screen, screen, "-R", "-D", NULL);
+	
+	// Are we being told to start sftp?
+	if(argc == 3 && strncmp(argv[1], "-c", 2) == 0)
+	{
+		// execute command
+		execlp(argv[2], argv[2], NULL);
+	}
+	else
+	{
+		// Screen per usual
+		execlp(screen, screen, "-R", "-D", NULL);
+	}
+	
 	return 0;
 }
 
