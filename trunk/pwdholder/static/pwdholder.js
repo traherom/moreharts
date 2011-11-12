1b5199a3005e0eeb2e853f67b4a7255df7f0135b
ryan@moreharts.com <ryan@moreharts.com@fa5ea75b-a228-d56f-84e4-a555cbe47481>
2011-11-12 21:40:19 +0000

    Local storage works on Android, remains on requested page when user is logged in and visits for the first time.

--- trunk/pwdholder/static/pwdholder.js
+++ trunk/pwdholder/static/pwdholder.js
@@ -13,7 +13,7 @@ var PwdHolder = function() {
 			// Checks if we (a) have a stored session, (b) if that stored session
 			// is valid still, and (c) who the session is for
 			if(sid == null) {
-				sid = localStorage["sid"]
+				sid = window["localStorage"]["sid"]
 				if(sid == null) {
 					callback(false, "No session stored");
 					return;
@@ -47,14 +47,14 @@ var PwdHolder = function() {
 					}
 					
 					// Save to permanent store
-					localStorage["sid"] = sid;
+					window["localStorage"]["sid"] = sid;
 				}
 			});
 		},
 		
 		logout: function(callback) {
 			// Remove local store
-			localStorage.removeItem("sid");
+			window["localStorage"].removeItem("sid");
 			
 			$.ajax({
 				url: "logout",
@@ -114,95 +114,3 @@ var PwdHolder = function() {
 	}
 }();
 
-$(document).ready(function() {
-	$("#login_form").submit(function() {
-		PwdHolder.login(
-			$("#master_user").val(),
-			$("#master_pw").val(),
-			function(success, data) {
-				if(success)
-					$.mobile.changePage("#get_password");
-				else
-					alert(data.message); // TBD: prettier
-			});
-		return false;
-	});
-	
-	$("#set_form").submit(function() {
-		PwdHolder.set(
-			$("#site_url_set").val(),
-			$("#site_user_set").val(),
-			$("#site_pw_set").val(),
-			function(success, data) {
-				if(success) {
-					// Immediately make call out to get the new values
-					$("#site_url_get").val($("#site_url_set").val());
-					$("#get_form").submit();
-					$.mobile.changePage("#get_password", { reverse: true });
-				}
-				else
-					alert(data.message); // TBD: prettier
-			});
-		return false;
-	});
-	
-	$("#get_form").submit(function() {
-		PwdHolder.get(
-			$("#site_url_get").val(),
-			function(success, data) {
-				if(success) {
-					$("#site_user_get_result").val(data.site_user);
-					$("#site_pw_get_result").val(data.site_pw);
-				}
-				else {
-					alert(data.message); // TBD: prettier
-				}
-			});
-		return false;
-	});
-	
-	$("#generate_form").submit(function() {
-		PwdHolder.generate(
-			$("#generate_min_len").val(),
-			$("#generate_max_len").val(),
-			$("#generate_include_punc").val(),
-			function(success, data) {
-				if(success)
-					$("#generate_pw_result").val(data.password);
-				else
-					$("#generate_pw_result").val(data.message);
-			});
-		return false;
-	});
-	
-	$("a[href=#login]").click(function(event) {
-		PwdHolder.logout(function() {
-			$.mobile.changePage("#login");
-		});
-		
-		event.preventDefault();
-	});
-	
-	// Easy copying for username/password
-	$("#copy_site_user").click(function() {
-		$("#site_user_get_result").select();
-	});
-	$("#copy_site_pw").click(function() {
-		$("#site_pw_get_result").select();
-	});
-	$("#copy_generated_pw").click(function() {
-		$("#generate_pw_result").select();
-	});
-	
-	// Choose starting page
-	PwdHolder.checkLogin(function(loggedIn, data) {
-		if(loggedIn) {
-			// Logged in!
-			$.mobile.changePage("#get_password");
-		}
-		else {
-			// Not logged in
-			$.mobile.changePage("#login");
-		}
-	});
-});
