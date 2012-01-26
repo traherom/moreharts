bab92f79d10c87f76b95d1827fb8e098543e1953
ryan <ryan@moreharts.com>
2012-01-26 03:12:27 +0000

    - Added time estimate to completion of hash insertion - Alter table to add/remove indexes for insert - Added two tables for storing a computer's state - Few small bug fixes for internet-less usage

--- hasher/check_hashes.py
+++ hasher/check_hashes.py
@@ -7,15 +7,28 @@ def check_hash(hashdb, hash):
     return False
 
 def check_dir(hashdb, dir):
-    for f_name in 
+	pass
 
 def main(argv):
     # Parse command line
     parser = argparse.ArgumentParser(description='Compares hashes of files on drive to hash database')
-    parser.add_argument('hashdb', meta='Hash DB', help='Text database of hashes to compare against')
-    parser.add_argument('root_dir', meta='Root', nargs='+', help='Directory to recursively hash files in')
+    parser.add_argument('--host', default='localhost', help='Host with the hash database')
+    parser.add_argument('--port', type=int, default=3389, help='Port for database')
+    parser.add_argument('-s', '-db', '--database', '--schema', default='hashes', help='Schema (database) on the server to use')
+    parser.add_argument('-u', '--user', required=True, help='Username for database')
+    parser.add_argument('-p', '--password', help='Password for database')
     parser.add_argument('-o', '--output', help='File to output results to')
+    parser.add_argument('--nsrl-compare', type=bool, action='store_true', default=True, help='Check files against NIST NSRL hashes (known-good files)')
+    parser.add_argument('--save-state', type=bool, action='store_true', default=False, help='Saves the hashes for this computer to the database')
+    parser.add_argument('--check-state', type=bool, action='store_true', default=False, help='Checks the hashes found against saved state. Shows additions, removals, and changes')
+    parser.add_argument('-i', '--identifier', help='Identifier for this computer')
+    parser.add_argument('root_dir', meta='Root', nargs='+', help='Directory to start recursive hashing of files')
     args = parser.parse_args()
+    
+    # If the user wants us to do a state action, they must give the ID
+    if (args.save_state or args.check_state) and args.identifier is None:
+    	print('A system identifier must be given to save or check state')
+    	return 1
     
     # Go through each directory the user requested
     for d in args.root_dir:
