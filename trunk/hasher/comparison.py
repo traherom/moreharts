3d673ee15a5608dcbd1ec58c8ba25c8bc2540044
ryan@moreharts.com <ryan@moreharts.com@fa5ea75b-a228-d56f-84e4-a555cbe47481>
2012-04-10 18:29:52 +0000

    Comparison tool can now save defaults for changes

--- trunk/hasher/comparison.py
+++ trunk/hasher/comparison.py
@@ -5,25 +5,40 @@ import hashlib
 import sys
 import shutil
 
-def prompt_for_action(left_path, right_path, default='i'):
-	promptStr = "Action? "
-	if default == 'i':
-		promptStr += '[c/d/I] '
-	elif default == 'd':
-		promptStr += '[c/D/i] '
-	elif default == 'c':
-		promptStr += '[C/d/i] '
+def prompt_for_action(settings, left_base, right_base, relative_path, default='i'):
+	# Is there a saved action for this path?
+	try:
+		choice = settings[relative_path]
+	except KeyError:
+		# There isn't, ask the user
+		promptStr = "Action? "
+		if default == 'i':
+			promptStr += '[c/d/I] '
+		elif default == 'd':
+			promptStr += '[c/D/i] '
+		elif default == 'c':
+			promptStr += '[C/d/i] '
 	
-	choice = input(promptStr)
-	if not choice:
-		choice = default
+		choice = input(promptStr)
+		if not choice:
+			choice = default
+		
+	# Normalize choice
+	choice = choice.lower()
+	if choice[0] == 'a':
+		# We're supposed to always take this action in the future
+		choice = choice[1]
+		
+		settings[relative_path] = choice
 
-	left_full = left_path		
-	right_full = right_path
-	
+	# Compose full path
+	left_full = os.path.join(left_base, relative_path)		
+	right_full = os.path.join(right_base, relative_path)
+
+	# Follow user's direction	
 	if choice == 'd':
 		# Delete
-		print(left_full, right_full)
+		print('Deleting', relative_path, 'from right')
 		
 		if os.path.isdir(right_full):
 			shutil.rmtree(right_full)
@@ -32,10 +47,14 @@ def prompt_for_action(left_path, right_path, default='i'):
 		
 	elif choice == 'c':
 		# Copy from left to right
+		print('Copying', relative_path, 'to right')
 		if os.path.isdir(right_full):
 			shutil.copytree(left_full, right_full)
 		else:
 			shutil.copy2(left_full, right_full)
+			
+	else:
+		print('Ignoring', relative_path)
 		
 def hash_file(path):
 	"""
@@ -76,7 +95,7 @@ def path_type(path):
 	else:
 		return t
 
-def walk(left_base, right_base, relative_path):
+def walk(settings, left_base, right_base, relative_path):
 	print('Working on {}'.format(relative_path), file=sys.stderr)
 
 	# Construct paths we're actually comparing
@@ -93,7 +112,7 @@ def walk(left_base, right_base, relative_path):
 	
 	if left_type != right_type:
 		print_diff(relative_path, 'Left is {}, right is {}'.format(left_type, right_type))
-		prompt_for_action(left_path, right_path, relative_path, 'd')
+		prompt_for_action(settings, left_base, right_base, relative_path, 'd')
 		return False
 	
 	if os.path.isfile(left_path):
@@ -103,7 +122,7 @@ def walk(left_base, right_base, relative_path):
 		
 		if lhash != rhash:
 			print_diff(relative_path, 'Hash does not match')
-			prompt_for_action(left_path, right_path, 'c')
+			prompt_for_action(settings, left_base, right_base, relative_path, 'c')
 			return False
 	elif os.path.isdir(left_path):
 		# Oh, we're a directory. Compare all our children
@@ -114,7 +133,7 @@ def walk(left_base, right_base, relative_path):
 		if not os.path.islink(left_path):
 			for child in sorted(os.listdir(left_path)):
 				next_relative = os.path.join(relative_path, child)
-				if not walk(left_base, right_base, next_relative):
+				if not walk(settings, left_base, right_base, next_relative):
 					matches = False
 
 		# Also, make sure the right side doesn't have extra files
@@ -122,23 +141,61 @@ def walk(left_base, right_base, relative_path):
 		if extra_right:
 			matches = False
 			for extra in sorted(extra_right):
-				print_diff(os.path.join(relative_path, extra), 'New on right')
+				extra_rel = os.path.join(relative_path, extra)
+				print_diff(extra_rel, 'New on right')
 				
 				# Delete new file?
-				prompt_for_action(os.path.join(left_path, extra), os.path.join(right_path, extra), 'd')
+				prompt_for_action(settings, left_base, right_base, extra_rel, 'd')
 				
 		return matches
 			
 	return True
 
+def save_settings(path, settings):
+	print("Saving settings...")
+	with open(path, 'w') as f:
+		for path, action in settings.items():
+			f.write('{} {}\n'.format(path, action))
+
+def load_settings(path):
+	print("Loading settings...")
+	settings = {}
+	try:
+		with open(path, 'r') as f:
+			for line in f:
+				# Skip blank lines (mostly the end)
+				line = line.strip()
+				if not line:
+					continue
+					
+				path = line[:-2]
+				action = line[-1]
+				settings[path] = action
+	except IOError as e:
+		# We're likely creating a new settings file
+		pass
+		
+	return settings
+
 def main(argv):
-	parser = argparse.ArgumentParser(description='''Displays differences between two directories''')
-	parser.add_argument('left', metavar='left side', help='Directory to compare against. The "control" in the experiment, if you will')
-	parser.add_argument('right', metavar='right side', help='Directory to compare')
+	parser = argparse.ArgumentParser(description='Displays differences between two directories')
+	parser.add_argument('--settings', '-s', help='File to save whether to always do ignore/copy/delete on certain paths')
+	parser.add_argument('left', metavar='<left>', help='Directory to compare against. The "control" in the experiment, if you will')
+	parser.add_argument('right', metavar='<right>', help='Directory to compare')
 	args = parser.parse_args()
 	
-	walk(args.left, args.right, '')
+	# Read in file that contains our "always do X" settings
+	settings = {}
+	if args.settings:
+		settings = load_settings(args.settings)
+	
+	# Do comparison
+	walk(settings, args.left, args.right, '')
 
+	# Save setting changes back
+	if args.settings:
+		save_settings(args.settings, settings)
+		
 	return 0
 
 if __name__ == '__main__':
