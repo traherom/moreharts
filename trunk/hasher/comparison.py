2d95f7fad5ad9ac8c7ab9d6ac498e64afd568a32
ryan@moreharts.com <ryan@moreharts.com@fa5ea75b-a228-d56f-84e4-a555cbe47481>
2012-05-21 20:13:51 +0000

    Added readme comment to top of file

--- trunk/hasher/comparison.py
+++ trunk/hasher/comparison.py
@@ -1,4 +1,76 @@
 #!/usr/bin/env python3
+#
+# Written by Ryan Morehart (ryan@moreharts.com) for CDX2012
+#
+# Simple comparison script that walks over two directories and displays the differences
+# between them (including file changes based on a SHA1 hash). It then provides the ability
+# to recitfy those differences using sane defaults (delete new files on the right,
+# copy changed files from left to right, and ignore files deleted on the right).
+#
+# Basic usage is simple:
+#		./comparison.py <left dir> <right dir>
+# 
+# The left dir is typically your "known good" directory and the right one is the one you
+# want to see the changes in. As comparison.py runs, it will prompt with changes that
+# it considers important and ask you what action to take:
+#	traherom@ramlingen:~/src/moreharts/hasher$ ./comparison.py test1 test2
+#	Working on 
+#	Working on aoeu
+#	aoeu: Does not exist on test2
+#	Working on crh
+#	crh: Hash does not match
+#	Action? [C/d/i] i
+#	Ignoring crh	
+#	Working on deeperstuff
+# 	Working on deeperstuff/aoeuaoeu
+#	deeperstuff/aoeuaoeu: Does not exist on test2
+#	deeperstuff/crchr: New on test2
+#	Action? [c/D/i] 
+#
+# comparison.py capitalize what it considers the best option (in this case, "copy"). Hitting
+# enter will accept that default, otherwise type the option you want and hit enter. The options
+# and the defaults are:
+#	-- c: Copy the file from left to right. Default for changed files
+#	-- d: Delete the file from right side. Default for new files (not present on left)
+#	-- i: Ignore file
+#
+# For large directories that will be compared more than once, saving settings (especially 
+# for files you wish to always ignore, such a registry hives) can be invaluable. There are
+# two things needed to do this. First, start comparison.py with --settings specified:
+#		./comparison.py --settings=<settingsfile> <left> <right>
+#
+# Then when prompted for a setting that you wish to save, prepend an 'a' to it (the
+# 'a' stands for "always," if you must know). 
+#	traherom@ramlingen:~/src/moreharts/hasher$ ./comparison.py --settings=settings.set test1 test2
+#	Loading settings...
+#	Working on 
+#	Working on aoeu
+#	aoeu: Does not exist on test2
+#	Working on crh
+#	crh: Hash does not match
+#	Action? [C/d/i] ai
+#	Ignoring crh
+#	Working on deeperstuff
+#	Working on deeperstuff/aoeuaoeu
+#	deeperstuff/aoeuaoeu: Does not exist on test2
+#	deeperstuff/crchr: New on test2
+#	Action? [c/D/i] ^C
+#	Do you want to save your settings? [Y/n] 
+#	Saving settings...
+#
+# In the future when you run with the same settings file, if comparison.py detects a difference
+# with a path you saved an action for, it will automatically do that without prompting (the
+# action is still logged, however). By manually editting this file, it also allows you to exclude
+# directories from comparison entirely. The format is just one line for every file and the associated
+# action. For example:
+#	file1 i
+# 	file2 c
+#	file3 d
+#
+# As a final note, actions taken (deletions, copies, etc) are sent to stdout while messages for
+# the user go to stderr. If you redirect the output somewhere, this will allow you to save
+# a log of the exact changes made while still monitoring progress and responding to prompts.
+#
 import os
 import argparse
 import hashlib
@@ -38,7 +110,7 @@ def prompt_for_action(settings, left_base, right_base, relative_path, default='i
 	# Follow user's direction	
 	if choice == 'd':
 		# Delete
-		print('Deleting', relative_path, 'from right')
+		print('Deleting', relative_path, 'from', right_base)
 		
 		if os.path.isdir(right_full):
 			shutil.rmtree(right_full)
@@ -47,7 +119,7 @@ def prompt_for_action(settings, left_base, right_base, relative_path, default='i
 		
 	elif choice == 'c':
 		# Copy from left to right
-		print('Copying', relative_path, 'to right')
+		print('Copying', relative_path, 'to', right_base)
 		if os.path.isdir(right_full):
 			shutil.copytree(left_full, right_full)
 		else:
@@ -117,11 +189,11 @@ def walk(settings, left_base, right_base, relative_path):
 	right_type = path_type(right_path)
 	
 	if right_type is None:
-		print_diff(relative_path, 'Does not exist on right side')
+		print_diff(relative_path, 'Does not exist on {}'.format(right_base))
 		return False
 	
 	if left_type != right_type:
-		print_diff(relative_path, 'Left is {}, right is {}'.format(left_type, right_type))
+		print_diff(relative_path, '{} is {}, {} is {}'.format(left_base, left_type, right_base, right_type))
 		prompt_for_action(settings, left_base, right_base, relative_path, 'd')
 		return False
 	
@@ -152,7 +224,7 @@ def walk(settings, left_base, right_base, relative_path):
 			matches = False
 			for extra in sorted(extra_right):
 				extra_rel = os.path.join(relative_path, extra)
-				print_diff(extra_rel, 'New on right')
+				print_diff(extra_rel, 'New on {}'.format(right_base))
 				
 				# Delete new file?
 				prompt_for_action(settings, left_base, right_base, extra_rel, 'd')
@@ -194,18 +266,37 @@ def main(argv):
 	parser.add_argument('right', metavar='<right>', help='Directory to compare')
 	args = parser.parse_args()
 	
-	# Read in file that contains our "always do X" settings
-	settings = {}
-	if args.settings:
-		settings = load_settings(args.settings)
+	try:
+		# Read in file that contains our "always do X" settings
+		settings = {}
+		if args.settings:
+			settings = load_settings(args.settings)
 	
-	# Do comparison
-	walk(settings, args.left, args.right, '')
-
-	# Save setting changes back
-	if args.settings:
-		save_settings(args.settings, settings)
+		# Do comparison
+		walk(settings, args.left, args.right, '')
 		
+		# Save setting changes back
+		if args.settings:
+			save_settings(args.settings, settings)
+
+	except BaseException as e:
+		# Real error or user asked us to die?
+		if type(e) is not KeyboardInterrupt:
+			print('ERROR: {}'.format(e), file=sys.stderr)
+			
+		# On any error (including keyboard interrupt), ask the user if
+		# they want to save their current settings
+		if args.settings and settings:
+			ans = input('\nDo you want to save your settings? [Y/n] ').lower()
+			if ans != 'n':
+				save_settings(args.settings, settings)
+			else:
+				print('Not saving settings', file=sys.stderr)
+		else:
+			# Ensure we nicely have a blank line to put the bash prompt
+			# in a good place
+			print(file=sys.stderr)
+
 	return 0
 
 if __name__ == '__main__':
